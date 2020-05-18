5b7d52b99998532095aecb99e55e63a4d0dac426
Anurag Dhiman <anuragdhiman400@gmail.com>
2020-05-18 16:07:44 -0400

    Update productDao.java

--- Spring_MVC_CRUD/src/com/Dao/productDao.java
+++ Spring_MVC_CRUD/src/com/Dao/productDao.java
@@ -3,12 +3,9 @@ package com.Dao;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
-
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-
 import com.Beans.Products;
-
 
 public class productDao {
 	JdbcTemplate template;  
